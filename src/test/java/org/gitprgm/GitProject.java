a631a0f869d328ae3c635c5aeb879f67803cb6b7
vijicaloo <vijicaloo@gmail.com>
2024-09-12 11:14:12 +0530

    First Sample

--- src/test/java/org/gitprgm/GitProject.java
+++ src/test/java/org/gitprgm/GitProject.java
@@ -3,19 +3,23 @@ package org.gitprgm;
 public class GitProject {
 	
 	private void abc() {
-	
+		System.out.println("abc");
 
-	}
+	}	
 	
 	private void ijk() {
-		
+		System.out.println("ijk");
 
 	}
 	
 	private void pqr() {
-		
+		System.out.println("pqr");
 	}
 	
 	public static void main(String[]args) {
+		GitProject g = new GitProject();
+		g.abc();
+		g.ijk();
+		g.pqr();
 }
 }
